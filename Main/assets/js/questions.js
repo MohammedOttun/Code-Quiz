00c1dcc0b1fc62c1da7bcabf02f65226a724d98c
MohammedOttun <ottunmo@mac.com>
2021-03-27 17:41:31 -0500

    Added questions array with: quesstion, choices and answer.

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question: "Why so JavaScript and Java have similar name?",
+        choices: ["JavaScript is a stripped-down version of Java", "JavaScript's syntax is loosely based on Java's", "They both originated on the island of Java", "None of the above"],
+        answer: "JavaScript's syntax is loosely based on Java's"
+    },
+    {
+        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        choices: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
+        answer: "The User's machine running a Web browser"
+    },
+    {
+        question: "______ JavaScript is also called client-side JavaScript.",
+        choices: ["Microsoft", "Navigator", "LiveWire", "Native"],
+        answer: "Navigator"
+    },
+    {
+        question: "__________ JavaScript is also called server-side JavaScript.",
+        choices: ["Microsoft", "Navigator", "LiveWire", "Native"],
+        answer: "LiveWire"
+    },
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        answer: "Storing numbers, dates, or other values"
+    },
+    {
+        question: "_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and ",
+        choices: ["page navigation", "Client-side", "Server-side", "Local", "Native"],
+        answer: "Client-side"
+    },
+    {
+        question: "What should appear at the very end of your JavaScript?\nThe \<script LANGUAGE\=\"JavaScript\"\>tag",
+        choices: ["The </script>", "The <script>", "The END statement", "None of the above"],
+        answer: "The </script>"
+    },
+    {
+        question: "Which of the following can't be done with client-side JavaScript?",
+        choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "None of the above"],
+        answer: "Storing the form's contents to a database file on the server"
+    },
+    {
+        question: "Which of the following are capabilities of functions in JavaScript?",
+        choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
+        answer: "Accept parameters"
+    },
+    {
+        question: "Which of the following is not a valid JavaScript variable name?",
+        choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+        answer: "2names"
+    }
+];
