693198c81415997b207c333f9172d84b0f0091d2
MohammedOttun <ottunmo@mac.com>
2021-03-28 14:22:41 -0500

    Added feedback element variable.
    Added question click logic that allows user to progress through questions after each guess.
    
    Completed question and answer options display rendering.

--- Main/assets/js/quiz.js
+++ Main/assets/js/quiz.js
@@ -6,7 +6,8 @@ var startButtom = document.getElementById("start");
 var submitButton = document.getElementById("submit");
 var initialsEl = document.getElementById("initials");
 var quizStartEl = document.getElementById("quiz-start");
-var quizEndEl = document.getElementById("quiz-end")
+var quizEndEl = document.getElementById("quiz-end");
+var feedbackEl = document.getElementById("feedback");
 
 // sound assets
 var rightSound = new Audio("../assets/audio/right.wav");
@@ -45,8 +46,63 @@ function getQuestions() {
 
     answerChoicesEL.innerHTML = "";
 
+    currentQuestion.choices.forEach(function(choice, i) { 
+        // button for each answer choice
+        var answerChoice = document.createElement("button");
+        answerChoice.setAttribute("class", "answer-choices");
+        answerChoice.setAttribute("value", choice);
+
+        answerChoice.textContent = i + 1 + ". " + choice;
+
+        // event listener for choice selection.
+        answerChoice.onclick = questionClick;
+
+        // display answer choices
+        answerChoicesEL.appendChild(answerChoice);
+     });
+
     console.log("Question to be displayed is captured");
 };
+
+function questionClick() {
+    // check for wrong guess
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      // 10-second penalty
+      time -= 10;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      timerEl.textContent = time;
+  
+      // play "wrong" sound effect
+      wrongSound.play();
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+      // play "right" sound effect
+      rightSound.play();
+  
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // flash feedback on page 
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestions();
+    }
+  };
 
 // clock ticker
 function clockTicker() {
